45f194255be0251a8511c6e424a33d0525dcf295
xuxy2640 <xuxy2640>
2022-06-05 23:39:53 +0800

    提交部署文件

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/bash
+
+## 打包
+echo '开始打包 >>> '
+cd ~/workspace/typrea-doc/ && npm run docs:build
+
+## 部署到node服务器
+
+echo '开始部署 >>> '
 rm -rf ~/workspace/nodeApp/public/docs/*  && cp -r  ~/workspace/typrea-doc/docs/.vuepress/dist/*  ~/workspace/nodeApp/public/docs/
+
+echo '部署完成 >>> '
+
